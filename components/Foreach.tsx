95525d665b95c5294b5c8de297296fdb06bfcadd
craftbox <bartoszdudzinski18@gmail.com>
2024-01-21 12:51:47 +0100

    added optional separator in foreach

--- components/Foreach.tsx
+++ components/Foreach.tsx
@@ -1,9 +1,18 @@
-import { ReactNode } from "react";
+import { ElementType, ReactNode } from "react";
 
-export default function Foreach<T>({ list, each }: { list: T[], each: (item: T, index: number, list: T[]) => ReactNode }) {
+export default function Foreach<T>({ list, each, separator: Separator }: { list: T[], each: (item: T, index: number, list: T[]) => JSX.Element, separator?: JSX.Element }) {
 	return (
 		<>
-			{list.map(each)}
+			{list.reduce((prev, v, i, arr) => {
+				prev.push(each(v, i, arr))
+				if (i < (arr.length - 1) && Separator) {
+					const separatorElem = Separator
+					separatorElem.key = prev.length.toString()
+					prev.push(separatorElem)
+				}
+				return prev
+			}, [] as ReactNode[])}
+			{/* {list.map(each)} */}
 		</>
 	)
 }
