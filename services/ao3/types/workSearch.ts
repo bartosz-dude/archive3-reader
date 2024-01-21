e126529c09f6f2320e0b40b94cfbebd8cc65eca0
craftbox <bartoszdudzinski18@gmail.com>
2024-01-21 18:41:12 +0100

    reversed enums

--- services/ao3/types/workSearch.ts
+++ services/ao3/types/workSearch.ts
@@ -1,6 +1,6 @@
-type Lang = "en"
-type Rating = "notRated" | "generalAudiences" | "teensAndUpAudiences" | "mature" | "explicit"
-type NumberSearch = `${number}` | `<${number}` | `>${number}` | `${number}-${number}`
+export type Lang = "en"
+export type Rating = "notRated" | "generalAudiences" | "teensAndUpAudiences" | "mature" | "explicit"
+export type NumberSearch = `${number}` | `<${number}` | `>${number}` | `${number}-${number}`
 
 export enum RatingIds {
 	notRated = 9,
@@ -8,6 +8,14 @@ export enum RatingIds {
 	teensAndUpAudiences = 11,
 	mature = 12,
 	explicit = 13
+}
+
+export const RatingIdsReversed = {
+	"9": "notRated",
+	"10": "generalAudiences",
+	"11": "teensAndUpAudiences",
+	"12": "mature",
+	"13": "explicit"
 }
 
 export enum SortByValues {
@@ -23,6 +31,19 @@ export enum SortByValues {
 	bookmarksCount = "bookmarks_count",
 }
 
+export const SortByValuesReversed = {
+	"_score": "bestMatch",
+	"authors_to_sort_on": "authors",
+	"title_to_sort_on": "title",
+	"created_at": "createdAt",
+	"revised_at": "revisedAt",
+	"word_count": "wordCount",
+	"hits": "hits",
+	"kudos_count": "kudosCount",
+	"comments_count": "commentsCount",
+	"bookmarks_count": "bookmarksCount",
+}
+
 export enum WarningsIds {
 	creatorChoseNotToUseArchiveWarnings = 14,
 	gaphicDepictionsOfViolence = 17,
@@ -32,6 +53,16 @@ export enum WarningsIds {
 	underage = 20
 }
 
+export const WarningsIdsReversed = {
+	"14": "creatorChoseNotToUseArchiveWarnings",
+	"17": "gaphicDepictionsOfViolence",
+	"18": "majorCharacterDeath",
+	"16": "noArchiveWarningsApply",
+	"19": "rapeNonCon",
+	"20": "underage"
+
+}
+
 export enum CategoriesIds {
 	fF = 116,
 	fM = 22,
@@ -39,6 +70,15 @@ export enum CategoriesIds {
 	mM = 23,
 	multi = 2246,
 	other = 24
+}
+
+export const CategoriesIdsReversed = {
+	"116": "fF",
+	"22": "fM",
+	"21": "gen",
+	"23": "mM",
+	"2246": "multi",
+	"24": "other"
 }
 
 type requiredAO3WorkSearchFields = "sortBy" | "sortDirection" | "completionStatus" | "crossovers"
@@ -89,5 +129,22 @@ export interface AO3WorkSearch {
 
 export interface AO3Query {
 	commit: "Search",
-	// TODO
+	"work_search[bookmarks_count]": NumberSearch
+	"work_search[character_names]": string
+	"work_search[comments_count]": NumberSearch
+	"work_search[complete]": string
+	"work_search[creators]": string
+	"work_search[crossover]": string
+	"work_search[fandom_names]": string
+	"work_search[freeform_names]": string
+	"work_search[hits]": NumberSearch
+	"work_search[kudos_count]": NumberSearch
+	"work_search[language_id]": Lang
+	"work_search[query]": string
+	"work_search[relationship_names]": string
+	"work_search[revised_at]": string
+	"work_search[sort_column]": string
+	"work_search[sort_direction]": string
+	"work_search[title]": string
+	"work_search[word_count]": NumberSearch
 }
