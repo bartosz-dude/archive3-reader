4a7abce7a7a3bbd05594a5b852551b000c144a2e
craftbox <bartoszdudzinski18@gmail.com>
2024-01-21 18:39:46 +0100

    basic error handling

--- services/saver/api/saveQuery.ts
+++ services/saver/api/saveQuery.ts
@@ -2,6 +2,11 @@ import fastHashCode from "fast-hash-code";
 import worksQuery from "../../ao3/api/worksQuery";
 import getDB from "../../database/getDB";
 
+export enum SaveQueryErrors {
+	alreadyExists = "entry already exists",
+	tableNotFound = "table not found"
+}
+
 export default async function saveQuery(query: ReturnType<typeof worksQuery>) {
 	console.log("save query")
 	const queryHash = fastHashCode(query.paramsAsJSON())
@@ -9,10 +14,16 @@ export default async function saveQuery(query: ReturnType<typeof worksQuery>) {
 	const db = getDB()
 
 	await db.transactionAsync(async tx => {
-		const a = await tx.executeSqlAsync("SELECT name FROM sqlite_master WHERE type='table'")
-		console.log("tables ", a.rows)
-		const checkResult = await tx.executeSqlAsync("SELECT hash FROM 'query_storage' WHERE hash = ?", [ queryHash ])
-		console.log("search ", checkResult)
-		await tx.executeSqlAsync("INSERT INTO 'query_storage' VALUES (?, ?)", [ queryHash, query.paramsAsJSON() ])
+		try {
+
+			await tx.executeSqlAsync("INSERT INTO 'query_storage' VALUES (?, ?)", [ queryHash, query.paramsAsJSON() ])
+		} catch (err) {
+			console.log("err", err)
+			if ((err as { message: string }).message.startsWith("UNIQUE constraint failed"))
+				throw Error(SaveQueryErrors.alreadyExists)
+
+			throw err
+		}
+		return
 	})
 }
