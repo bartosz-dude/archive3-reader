40c11c327e1b324dd18ad4807f125657915f8802
craftbox <bartoszdudzinski18@gmail.com>
2024-01-22 20:25:33 +0100

    added support for default search page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,11 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import { Text, View } from "react-native";
+import { useSettings } from "../../services/appSettings/components/settingsProvider";
 
 export default function TabsLayout() {
+
+	const { settings } = useSettings()
 
 	return (
 		<>
@@ -22,13 +25,20 @@ export default function TabsLayout() {
 				<Tabs.Screen
 					name="search"
 					options={{
-						href: "/search",
+						href: settings.savedSearchesAsDefault ? "/search/savedSearches" : "/search",
 						title: "Search",
 						tabBarIcon(props) {
 							return <MaterialCommunityIcons name="magnify" size={props.size} color={props.color} />
 						},
 					}}
 				/>
+				{/* <Tabs.Screen
+					name="saved/index"
+					options={{
+						href: "/saved",
+						title: "Saved"
+					}}
+				/> */}
 			</Tabs>
 		</>
 	)
