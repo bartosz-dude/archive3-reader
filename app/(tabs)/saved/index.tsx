dfa1f86ffa2b1c44488cdba6bc038bb51f97b1f1
craftbox <bartoszdudzinski18@gmail.com>
2024-02-02 21:42:33 +0100

    saved works

--- app/(tabs)/saved/index.tsx
+++ app/(tabs)/saved/index.tsx
@@ -1,14 +1,31 @@
-import { Text } from "react-native";
-import { useSettings } from "../../../services/appSettings/components/settingsProvider";
-import Foreach from "../../../components/common/Foreach";
+import { Text } from "react-native"
+import { useSettings } from "../../../services/appSettings/components/settingsProvider"
+import Foreach from "../../../components/common/Foreach"
+import useLoading from "../../../hooks/useLoading"
+import getAllWorks from "../../../services/saver/database/getAllWorks"
+import Loaded from "../../../components/common/Loaded"
+import { useEffect } from "react"
+import useCleanup from "../../../hooks/useCleanup"
 
 export default function SavedPage() {
+	// const { settings } = useSettings()
+	const works = useLoading(() => getAllWorks())
 
-	const { settings } = useSettings()
+	// useEffect(() => {
+	// 	if (works.status == "loaded") console.log(works.data)
+	// }, [works])
 
 	return (
 		<>
 			<Text>Saved Page</Text>
+			<Loaded isLoading={works.status}>
+				<Foreach
+					list={works.data ?? []}
+					each={(item, i) => {
+						return <Text key={item.workId}>Id: {item.workId}</Text>
+					}}
+				/>
+			</Loaded>
 		</>
 	)
 }
