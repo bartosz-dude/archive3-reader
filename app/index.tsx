5a21c0aa3a29a27b45b084ad4a06e1b23a84058e
craftbox <bartoszdudzinski18@gmail.com>
2024-02-02 21:29:44 +0100

    changed to redirect

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,15 @@
-import 'react-native-url-polyfill/auto';
-import { Link } from "expo-router";
-import { View, StyleSheet } from "react-native";
-import workSearchURL from "../services/ao3/tools/workSearchUrl";
+import "react-native-url-polyfill/auto"
+import { Link, Redirect } from "expo-router"
+import { View, StyleSheet } from "react-native"
+import workSearchURL from "../services/ao3/tools/workSearchUrl"
 import useAsyncMemo from "../hooks/useAsyncMemo"
-import { useEffect } from "react";
-import { useSettings } from "../services/appSettings/components/settingsProvider";
+import { useEffect } from "react"
+import { useSettings } from "../services/appSettings/components/settingsProvider"
 
 export default function RootIndex() {
-
-	const { settings } = useSettings()
-
 	return (
 		<>
-			<View style={styles.container}>
-				<Link href={"/work/48613378/chapter/122623150"} style={{ height: 50, width: 50, backgroundColor: "blue", color: "white" }}>Works</Link>
-				<Link href={settings.savedSearchesAsDefault ? "/search/savedSearches" : "/search"} style={{ height: 50, width: 50, backgroundColor: "blue", color: "white" }}>Search</Link>
-			</View>
+			<Redirect href={"/search"} />
 		</>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center"
-	}
-})
