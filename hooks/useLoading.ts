43a4d94083842676f5f71f258d8ed655b901eec6
craftbox <bartoszdudzinski18@gmail.com>
2024-02-02 21:41:27 +0100

    added reload

--- hooks/useLoading.ts
+++ hooks/useLoading.ts
@@ -1,10 +1,11 @@
 import { useEffect, useMemo, useState } from "react"
 
-export default function useLoading<T extends any>(loader: () => Promise<T>, deps: React.DependencyList) {
+export default function useLoading<T extends any>(loader: () => Promise<T>, deps?: React.DependencyList) {
 
 	const [ data, setData ] = useState<T | null>(null)
 	const [ status, setStatus ] = useState<"loading" | "loaded" | "failed">("loading")
 	const [ error, setError ] = useState<any | null>(null)
+	const [ updater, setUpdater ] = useState(false)
 
 	useMemo(() => {
 		setStatus("loading")
@@ -20,7 +21,11 @@ export default function useLoading<T extends any>(loader: () => Promise<T>, deps
 				setError(v)
 				setStatus("failed")
 			})
-	}, deps)
+	}, [ ...(deps ?? []), updater ])
 
-	return { data: data, error: error, status: status } as { data: typeof data, error: typeof error, status: typeof status }
+	function reload() {
+		setUpdater(prev => !prev)
+	}
+
+	return { data, error, status, reload }
 }
