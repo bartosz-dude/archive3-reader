a7e89d2358c5c76ffa89aedc030c6715252655f1
craftbox <bartoszdudzinski18@gmail.com>
2024-02-03 01:32:50 +0100

    added text props

--- components/common/Btn.tsx
+++ components/common/Btn.tsx
@@ -1,29 +1,54 @@
-import { Pressable, PressableProps, StyleProp, StyleSheet, Text, ViewStyle } from "react-native";
+import {
+	Pressable,
+	PressableProps,
+	StyleProp,
+	StyleSheet,
+	Text,
+	TextProps,
+	TextStyle,
+	ViewStyle,
+} from "react-native"
 
-export default function Btn({ children, style, disabled, onPress }: { children: string, style?: StyleProp<ViewStyle>, disabled?: PressableProps[ "disabled" ], onPress?: PressableProps[ "onPress" ] }) {
-
+export default function Btn({
+	children,
+	style,
+	textStyle,
+	disabled,
+	onPress,
+	...textProps
+}: {
+	children: string | string[]
+	style?: StyleProp<ViewStyle>
+	textStyle?: StyleProp<TextStyle>
+	disabled?: PressableProps["disabled"]
+	onPress?: PressableProps["onPress"]
+} & Omit<TextProps, "style" | "disabled" | "onPress">) {
 	const styleLocal = StyleSheet.create({
 		view: {
 			display: "flex",
 			justifyContent: "center",
 			alignItems: "center",
-			alignSelf: "stretch"
+			alignSelf: "stretch",
 		},
 		text: {
 			// display
 			// textAlign: "center",
 			// textAlignVertical: "center"
-
-		}
+		},
 	})
 
 	return (
 		<Pressable
-			style={[ styleLocal.view, style ]}
+			style={[styleLocal.view, style]}
 			onPress={onPress}
 			disabled={disabled}
 		>
-			<Text style={[ styleLocal.text ]}>{children}</Text>
+			<Text
+				style={[styleLocal.text, textStyle]}
+				{...textProps}
+			>
+				{children}
+			</Text>
 		</Pressable>
 	)
 }
