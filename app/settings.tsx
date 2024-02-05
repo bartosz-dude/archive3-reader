7e3fe2b1fd73501bf86c6f0b23931179eb642e6a
craftbox <bartoszdudzinski18@gmail.com>
2024-02-05 21:33:19 +0100

    new settings ui

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,33 +1,60 @@
 import { Switch, Text, View } from "react-native"
 import { useSettings } from "../services/appSettings/components/settingsProvider"
 import Foreach from "../components/common/Foreach"
+import Header from "../components/common/Header"
+import BackBtn from "../components/common/BackBtn"
+import SettingsSwitch from "../components/settings/settingsSwitch"
+import useStyle from "../hooks/useStyle"
+import { ScrollView } from "react-native"
+import { Link } from "expo-router"
 
 export default function SettingsPage() {
-
 	const { settings, update } = useSettings()
+
+	const style = useStyle({
+		settingsList: {
+			paddingHorizontal: 10,
+			paddingVertical: 10,
+		},
+		aboutView: {
+			paddingHorizontal: 10,
+			paddingVertical: 10,
+		},
+		aboutList: {
+			display: "flex",
+			gap: 5,
+			paddingTop: 5,
+		},
+	})
 
 	return (
 		<>
-			<Text>Settings</Text>
-			<Text>Saved searches as default when opening search tab</Text>
-			<Switch
-				onValueChange={() => update({ savedSearchesAsDefault: !settings.savedSearchesAsDefault })}
-				value={settings.savedSearchesAsDefault}
-			/>
-			{/* <Foreach
-				list={Object.entries(settings)}
-				each={([ k, v ], i) => {
-					// console.log(k, v)
-					return (
-						<View key={i}>
-							<Text>{k}</Text>
-							<Switch
-								value={v}
-							/>
-						</View>
-					)
-				}}
-			/> */}
+			<Header>
+				<BackBtn />
+				<Text style={{ color: "white" }}>Settings</Text>
+				<View style={{ width: 32 }} />
+			</Header>
+			<ScrollView>
+				<View style={style.settingsList}>
+					<SettingsSwitch
+						text="Saved searches as default when opening search tab"
+						value={settings.savedSearchesAsDefault}
+						onValueChange={() =>
+							update({
+								savedSearchesAsDefault:
+									!settings.savedSearchesAsDefault,
+							})
+						}
+					/>
+				</View>
+				<View style={style.aboutView}>
+					<Text>About</Text>
+					<View style={style.aboutList}>
+						<Link href={`/packages`}>Packages</Link>
+						<Link href={`/license`}>License</Link>
+					</View>
+				</View>
+			</ScrollView>
 		</>
 	)
 }
---
+++ components/settings/settingsSwitch.tsx
@@ -0,0 +1,27 @@
+import { Switch, Text, View } from "react-native"
+import useStyle from "../../hooks/useStyle"
+
+export default function SettingsSwitch(props: {
+	text: string
+	value: boolean
+	onValueChange: (value: boolean) => void | Promise<void>
+}) {
+	const style = useStyle({
+		entry: {
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "space-between",
+			alignItems: "center",
+		},
+	})
+
+	return (
+		<View style={style.entry}>
+			<Text>{props.text}</Text>
+			<Switch
+				value={props.value}
+				onValueChange={props.onValueChange}
+			/>
+		</View>
+	)
+}
