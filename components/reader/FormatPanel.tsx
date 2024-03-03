43d930f092f09cce70e824794e25097b7b1ebd39
craftbox <bartoszdudzinski18@gmail.com>
2024-03-03 20:33:27 +0100

    added spacing settings

--- components/reader/FormatPanel.tsx
+++ components/reader/FormatPanel.tsx
@@ -1,20 +1,21 @@
 import Slider from "@react-native-community/slider"
-import { Text, View } from "react-native"
+import { ScrollView, Text, View } from "react-native"
 import Btn from "../common/Btn"
 import Show from "../common/Show"
 import { FontWeight, useFormatter } from "./ReaderFormatter"
 import { useAppTheme } from "../ThemeManager"
 import useStyle from "../../hooks/useStyle"
 import { useState } from "react"
+import FormatSlider from "./FormatSlider"
 
 export default function FormatPanel(props: { isOpened: boolean }) {
 	const theme = useAppTheme()
 	const formatter = useFormatter()
 
 	// const [formatEditorOpened, setFormatEditorOpened] = useState(false)
-	const [formatEditorTab, setFormatEditorTab] = useState<"fonts" | "spacing">(
-		"fonts"
-	)
+	const [formatEditorTab, setFormatEditorTab] = useState<
+		"fonts" | "spacing" | "display"
+	>("fonts")
 
 	const style = useStyle({
 		content: {
@@ -40,7 +41,7 @@ export default function FormatPanel(props: { isOpened: boolean }) {
 		formatEditor: {
 			position: "absolute",
 			backgroundColor: theme.header.menuBackground,
-			// height: 200,
+			height: 210,
 			width: "100%",
 			bottom: 0,
 		},
@@ -53,6 +54,16 @@ export default function FormatPanel(props: { isOpened: boolean }) {
 		},
 		formatEditorContent: {
 			paddingHorizontal: 15,
+			display: "flex",
+			flexDirection: "row",
+			gap: 15,
+		},
+		formatEditorLabelColumn: {
+			display: "flex",
+			justifyContent: "space-around",
+		},
+		formatEditorSliderColumn: {
+			flexGrow: 1,
 		},
 		slider: {
 			display: "flex",
@@ -84,7 +95,7 @@ export default function FormatPanel(props: { isOpened: boolean }) {
 							>
 								Fonts
 							</Btn>
-							{/* <Btn
+							<Btn
 								textStyle={{
 									color: theme.header.font,
 									borderBottomColor: theme.header.accent,
@@ -94,65 +105,129 @@ export default function FormatPanel(props: { isOpened: boolean }) {
 								onPress={() => setFormatEditorTab("spacing")}
 							>
 								Spacing
-							</Btn> */}
+							</Btn>
+							<Btn
+								textStyle={{
+									color: theme.header.font,
+									borderBottomColor: theme.header.accent,
+									borderBottomWidth:
+										formatEditorTab == "display" ? 1 : 0,
+								}}
+								onPress={() => setFormatEditorTab("display")}
+							>
+								Display
+							</Btn>
 						</View>
-						<View style={style.formatEditorContent}>
+						<ScrollView
+							// style={}
+							contentContainerStyle={style.formatEditorContent}
+							persistentScrollbar
+						>
 							<Show when={formatEditorTab == "fonts"}>
-								<View style={style.slider}>
+								<View style={style.formatEditorLabelColumn}>
 									<Text>Font size</Text>
-									<Slider
-										style={{ flexGrow: 1 }}
-										minimumValue={10}
-										maximumValue={32}
-										value={formatter.format.fontSize}
-										step={2}
-										thumbTintColor={theme.tabBar.selected}
-										minimumTrackTintColor={
-											theme.tabBar.selected
-										}
-										maximumTrackTintColor={
-											theme.tabBar.unselected
-										}
-										onValueChange={(e) => {
+								</View>
+								<View style={style.formatEditorSliderColumn}>
+									<FormatSlider
+										buttonStep={2}
+										max={32}
+										min={10}
+										valueState={formatter.format.fontSize}
+										sliderStep={1}
+										valueDisplay={formatter.format.fontSize}
+										onChange={(e) => {
 											formatter.update({
 												fontSize: e,
 											})
 										}}
 									/>
-									<Text>{formatter.format.fontSize}</Text>
-								</View>
-								<View style={style.slider}>
-									<Text>Font weight</Text>
-									<Slider
-										style={{ flexGrow: 1 }}
-										minimumValue={0}
-										maximumValue={900}
-										value={formatter.format.fontWeight}
-										step={100}
-										thumbTintColor={theme.tabBar.selected}
-										minimumTrackTintColor={
-											theme.tabBar.selected
-										}
-										maximumTrackTintColor={
-											theme.tabBar.unselected
-										}
-										onValueChange={(e) => {
-											formatter.update({
-												fontWeight: e as FontWeight,
-											})
-										}}
-									/>
-									<Text>
-										{formatter.format.fontWeight > 0
-											? formatter.format.fontWeight / 100
-											: 0}
-									</Text>
 								</View>
 							</Show>
 							<Show when={formatEditorTab == "spacing"}>
-								<Text>spacing</Text>
+								<View style={style.formatEditorLabelColumn}>
+									<Text>Paragraph spacing</Text>
+									<Text>Side margin</Text>
+									<Text>Line spacing</Text>
+									{/* <Text>Word spacing</Text> */}
+								</View>
+								<View style={style.formatEditorSliderColumn}>
+									<FormatSlider
+										buttonStep={1}
+										max={20}
+										min={1}
+										valueState={
+											formatter.format.paragraphSpacing
+										}
+										sliderStep={1}
+										valueDisplay={formatter.format.fontSize}
+										onChange={(e) => {
+											formatter.update({
+												paragraphSpacing: e,
+											})
+										}}
+									/>
+									<FormatSlider
+										buttonStep={5}
+										max={20}
+										min={1}
+										valueState={
+											formatter.format.horizontalSpacing
+										}
+										sliderStep={1}
+										valueDisplay={
+											formatter.format.horizontalSpacing
+										}
+										onChange={(e) => {
+											formatter.update({
+												horizontalSpacing: e,
+											})
+										}}
+									/>
+									<FormatSlider
+										buttonStep={2}
+										max={16}
+										min={1}
+										valueState={
+											formatter.format.lineSpacing
+										}
+										sliderStep={1}
+										valueDisplay={
+											formatter.format.lineSpacing
+										}
+										onChange={(e) => {
+											formatter.update({
+												lineSpacing: e,
+											})
+										}}
+									/>
+									{/* <FormatSlider
+										buttonStep={1}
+										max={5}
+										min={1}
+										valueState={
+											formatter.format.wordSpacing
+										}
+										sliderStep={1}
+										valueDisplay={
+											formatter.format.wordSpacing
+										}
+										onChange={(e) => {
+											formatter.update({
+												wordSpacing: e,
+											})
+										}}
+									/> */}
+								</View>
 							</Show>
-						</View>
+							<Show when={formatEditorTab == "display"}>
+								<View style={style.formatEditorLabelColumn}>
+									<Text>Background color</Text>
+									<Text>Scrolling direction</Text>
+									<Text>Scrolling type</Text>
+								</View>
+								{/* <Text>Display</Text> */}
+							</Show>
+						</ScrollView>
 					</View>
 				</View>
 			</Show>
