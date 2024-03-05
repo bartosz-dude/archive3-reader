6781642b49c5bc41cdae594d40b7a5da95dea6d6
craftbox <bartoszdudzinski18@gmail.com>
2024-03-05 20:40:35 +0100

    updated formatting

--- services/appSettings/components/settingsProvider.tsx
+++ services/appSettings/components/settingsProvider.tsx
@@ -1,36 +1,37 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { Settings } from "../types/settings";
-import useLoading from "../../../hooks/useLoading";
-import getSettings from "../api/getSettings";
-import saveSettings from "../api/saveSettings";
+import { createContext, useContext, useEffect, useState } from "react"
+import { Settings } from "../types/settings"
+import useLoading from "../../../hooks/useLoading"
+import getSettings from "../api/getSettings"
+import saveSettings from "../api/saveSettings"
+import { DeepPartial } from "../../../types/common"
 
 interface SettingsContextData {
-	settings: Settings,
-	update: (newSettings: Partial<Settings>) => void
+	settings: Settings
+	update: (newSettings: DeepPartial<Settings>) => void
 }
 
 // @ts-ignore
 const SettingsContext = createContext<SettingsContextData>(null)
 
 export function SettingsProvider(props: { children: JSX.Element }) {
+	const [settingsData, setSettingsData] = useState<Settings | null>(null)
+	const [updater, setUpdater] = useState(false)
 
-	const [ settingsData, setSettingsData ] = useState<Settings | null>(null)
-	const [ updater, setUpdater ] = useState(false)
-
-	const settings = useLoading(getSettings, [ updater ])
+	const settings = useLoading(getSettings, [updater])
 
 	useEffect(() => {
-		if (settings.data)
-			setSettingsData(settings.data)
-	}, [ settings.data ])
+		if (settings.data) setSettingsData(settings.data)
+	}, [settings.data])
 
-	const update = (newSettings: Partial<Settings>) => {
-		saveSettings(newSettings).then(() => setUpdater(prev => !prev))
+	const update = (newSettings: DeepPartial<Settings>) => {
+		saveSettings(newSettings).then(() => setUpdater((prev) => !prev))
 	}
 
 	if (settingsData)
 		return (
-			<SettingsContext.Provider value={{ settings: settingsData, update: update }}>
+			<SettingsContext.Provider
+				value={{ settings: settingsData, update: update }}
+			>
 				{props.children}
 			</SettingsContext.Provider>
 		)
