2d4d4fb6fd10aa92f891f7ebb7aadfbe3784b8f8
craftbox <bartoszdudzinski18@gmail.com>
2024-03-05 20:41:04 +0100

    updated to use deepMerge

--- services/appSettings/api/saveSettings.ts
+++ services/appSettings/api/saveSettings.ts
@@ -1,13 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { Settings } from "../types/settings"
 import getSettings from "./getSettings"
+import { DeepPartial } from "../../../types/common"
+import { mergeDeep } from "../../../tools/deepMerge"
 
-export default async function saveSettings(newSettings?: Partial<Settings>) {
+export default async function saveSettings(newSettings: DeepPartial<Settings>) {
 	const prevSettings = await getSettings()
-	const settings: Settings = {
-		...prevSettings,
-		...newSettings,
-	}
+	const settings: Settings = mergeDeep(prevSettings, newSettings)
 
 	await AsyncStorage.setItem("settings", JSON.stringify(settings))
 }
